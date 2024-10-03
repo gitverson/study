37e8eea6e650b9b3bb95122993593b8c16e1dc35
Nawal Kishor <nawal.shakya@gmail.com>
2024-10-03 15:36:06 +0530

    test

--- file_get_contents.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-#how to fetch data from another website in php
-
-$content = file_get_contents("https://www.google.com");
-echo $content; 
-?>
--- funtionCallByRefrenece.php
+++
@@ -1,18 +0,0 @@
-<?php
-
-//You can pass a variable by reference to a function so the function can modify the variable.
-//Function call by reference
-	function add(&$x)
-	{
-		$x=$x+20;
-	}
-	
-$a=10;
-echo "$a <br/>";
- add($a);
-echo "$a <br/>";
-
-//echo "$x <br/>";
-
-#######################################################################################################
-
